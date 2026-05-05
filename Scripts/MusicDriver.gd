bb971151416b8e59b3542ff1f51a687cf7bd0f86
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2026-05-05 09:01:11 -0500

    h3 - Transitions
    Did all the work to make the tracks transition correctly from state to state. Lots of bitwise code. Next up is actually recording the chart - tonight I hope to get the music tracks in, but that might be wishful thinking.

--- Scripts/MusicDriver.gd
+++ Scripts/MusicDriver.gd
@@ -12,6 +12,8 @@ var pulse := 0.0
 @export_flags_2d_render var state:int : set = set_state
 ## The tracks that are in the middle of transitioning.
 var untransitioned:int = 0
+## The beat the transition starts from.
+var transition_start_beat:int = 0
 
 ## How many beats it takes to fade in or out a track.
 @export var transition_beats := 8
@@ -36,21 +38,43 @@ func _ready() -> void:
 	
 	play()
 
-func _process(delta: float) -> void:
-	beat = (get_playback_position() +  AudioServer.get_time_since_last_mix()) * bpm * delta
+func _process(_delta: float) -> void:
+	beat = (get_playback_position() +  AudioServer.get_time_since_last_mix()) * bpm / 60
 	pulse = beat - floor(beat)
 	
-	var index := 1
-	for i in range(31): 
-		if untransitioned & index:
-			print(i)
-		index *= 2
+	if stream is AudioStreamSynchronized:
+		var index := 1
+		
+		# How close to the end of the transition (0.0 - 1.0) we are.
+		var transition_time := (beat - transition_start_beat) / transition_beats
+		
+		for i in range(31): 
+			if untransitioned & index:
+				
+				var new_value:float = clamp(transition_time if state & index else 1 - transition_time, 0., 1.)
+				
+				stream.set_sync_stream_volume(i, linear_to_db(new_value))
+				
+				# If the value is what it's supposed to be, erase
+				# This index from the untransitioned list.
+				if new_value == (state & index) >> (i):
+					untransitioned ^= index
+			
+			index *= 2
+	
+	## Every half beat.
+	if floor(beat * 2) / 2 != last_beat:
+		last_beat = floor(beat * 2) / 2
+		print(last_beat)
+
+var last_beat := 0.0
+var string := ""
 
 func set_state(to:int):
 	
 	## Note which bits have changed, since they'll need to be transitioned.
 	untransitioned = state ^ to
 	
-	print(state, " -> ", to, " = ", untransitioned)
+	transition_start_beat = ceil(beat)
 	
 	state = to
